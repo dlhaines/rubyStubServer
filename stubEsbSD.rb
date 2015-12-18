0e1ea6cc0e7bd10b7636ea79ec9881004539b6d1
David Haines <dlhaines@umich.edu>
2015-12-18 11:45:05 -0500

    working version but for query parameters

--- stubEsbSD.rb
+++ stubEsbSD.rb
@@ -5,35 +5,6 @@ require 'sinatra'
 require 'slim'
 require 'json'
 
-## api description
-@@apidoc = <<END
-<html>
-<body)
-<p/>
-HOST: - Returns stub data for requests to the StudentDashboard API.
-It recognizes requests of the form:
- <p/>
-/StudentDashboard/v1/Students/:name/Terms/:terms/Schedule' 
-and 
-<p/>
-/StudentDashboard/v1/Students/:name/Terms'
-<p/>
-The values in the URL corresponding to :name and :terms value will be used to
-check for a file with a matching name.  E.g. A request for /StudentDashboard/v1/Students/kermit/Terms/2010/Schedule
-would return the contents of a file named data/kermit.2010.schedule.data if it exists.  Otherwise
-it will return the contents of data/default.schedule.data.
-<p/>
-The data directory will be in the same directory as the server.
-</body>
-</html>
-END
-
-## respond to request for api description.
-get '/api' do
-  content_type :html
-  @@apidoc
-end
-
 #respond to schedule request
 #https://woodpigeon.dsc.umich.edu:8243/StudentDashboard/v1/Students/ststvii/Terms/2010/Schedule
 
---
+++ stubToDisk.rb
@@ -0,0 +1,95 @@
+# Map url to disk files.  Path elements of the url map to directories. Final element maps
+# to a file.  Query parameters map to parts of the requested file name.  If directory is found
+# but no matching file then the default file in that directgry is returned. default.json is returned.
+# If no file is found then return 404.
+# Only handles json files for now.
+
+# TTD:
+# does not use query parameters.
+
+require 'sinatra'
+require 'slim'
+require 'json'
+
+set :base_dir, "./data"
+
+helpers do
+
+  # find the name of the corresponding file on disk using base_dir and defaulting file name.
+  # return nil if no matching file is found.  It assumes no query parameters.
+  def getDiskFileName(file_path, file_name)
+
+    puts "dfn: file_path: [#{file_path}] file_name: [#{file_name}]"
+    full_file_path = settings.base_dir
+
+    if !file_path.nil? && file_path.length > 0
+      full_file_path = "#{full_file_path}#{file_path}"
+    end
+
+    if File.exists? ("#{full_file_path}/#{file_name}")
+      return "#{full_file_path}/#{file_name}"
+    end
+
+    if File.exists? ("#{full_file_path}/default.json")
+      return "#{full_file_path}/default.json"
+    end
+
+    nil
+  end
+
+  # actually return a disk file unless there isn't one.
+  def getDiskFile(full_disk_file)
+    unless full_disk_file.nil?
+      return File.read(full_disk_file)
+    end
+
+    halt 404
+  end
+
+end
+
+# testing
+get '/' do
+  "@base_dir: [#{settings.base_dir}]"
+end
+
+# for all queries find the associated file.
+get '*' do
+  content_type :json
+
+  path_info = request.env["PATH_INFO"]
+  rx = Regexp.new("^(.*)/([^/]*)$")
+  file_match = rx.match(path_info)
+
+  file_path = file_match[1]
+  file_name = file_match[2]
+
+  file_request = getDiskFileName(file_path, file_name)
+
+  getDiskFile(file_request)
+end
+
+
+# def getWrappedDiskFile(data_file)
+#   logger.debug "#{__method__}: #{__LINE__}: DPFC: data file string: "+data_file
+#   if File.exists?(data_file)
+#     logger.debug "#{__method__}: #{__LINE__}: DPFC: file exists: #{data_file}"
+#     classes = File.read(data_file)
+#
+#     wrapped = WAPIResultWrapper.value_from_json(classes);
+#
+#     # if it isn't valid then just wrap what did come back.
+#     unless wrapped.valid?
+#       wrapped = WAPIResultWrapper.new(200, "found file #{data_file}", classes)
+#     end
+#
+#   else
+#     logger.debug "#{__method__}: #{__LINE__}: DPFC: file does not exist: #{data_file}"
+#     wrapped = WAPIResultWrapper.new(404, "File not found", "Data provider from files did not find a matching file for #{data_file}")
+#   end
+#
+#   logger.debug "#{__method__}: #{__LINE__}: DPFC: returning: "+wrapped.to_s
+#   return wrapped
+# end
+
+## end
